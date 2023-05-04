c9cb58db610927871e77031c717ae22906fb3338
BDA2013 <129307670+BDA2013@users.noreply.github.com>
2023-05-04 18:29:33 -0400

    choices merged together

--- assests/script.js
+++ assests/script.js
@@ -1,13 +1,16 @@
 // Assignment code here
 function generatePassword() {
   // Index 0-25 are for letters. 26-35 are for numbers
-  var alphabetNumericCharacters = 'abcdefghijklmnopqrstuvwxyz0123456789'.split("");
+  var characters;
+  var alphaCharacters = 'abcdefghijklmnopqrstuvwxyz'.split("");
+  var numCharacters = '0123456789'.split("");
   var specialCharacters = [" ", "!", '"', "#", "$", "%", "&", "'", "(", ")", "*", "+", ",", "-", ".",
-                           "/", ":", ";", "<", "=", ">", "?", "@", "[", "]", "^", "_", "`", "{", "|", 
+                           "/", ":", ";", "<", "=", ">", "?", "@", "[", "\\", "]", "^", "_", "`", "{", "|", 
                            "}", "~"];
+  var password = [];
 
   var userCharacters = prompt("How many characters you want?");
-  characters = parseInt(userCharacters);
+  var characters = parseInt(userCharacters);
 
   while(characters < 8 || characters > 128) {
     alert("Invalid number");
@@ -15,16 +18,51 @@ function generatePassword() {
     characters = parseInt(userCharacters);
   }
 
-  var userLowPassword = prompt("Do you want lowercase letters?");
-  //var selectLowPassword = userLowPassword;
-  var userUpPassword = prompt("Do you want Uppercase letters?");
-  //var selectUPPassword = userUpPassword;
-  var userNumericPassword = prompt("Do you want numeric characters?");
-  //var selectNumericPassword = userNumericPassword;
-  var userSpecial = prompt("Do you want special characters?");
-  //var selectSpecial = userSpecial;
+  var confirmLowPassword = prompt("Do you want lowercase letters?");
+  var confirmUpPassword = prompt("Do you want Uppercase letters?");
+  var confirmNumericPassword = prompt("Do you want numeric characters?");
+  var confirmSpecialPassword = prompt("Do you want special characters?");
 
+  //Gathering the user choices
+  while (confirmLowPassword != 'y' || 'n') {
+    alert("y or n only");
+    confirmLowPassword = prompt("Do you want lowercase letters?");
+  };
 
+  while (confirmUpPassword != 'y' || 'n') {
+    alert("y or n only");
+    confirmUpPassword = prompt("Do you want Uppercase letters?");
+    
+  };
+
+  while (confirmNumericPassword != 'y' || 'n') {
+    alert("y or n only");
+    confirmNumericPassword = prompt("Do you want numeric characters?");
+  };
+
+  while (confirmSpecialPassword != 'y' || 'n') {
+    alert("y or n only");
+    confirmSpecialPassword = prompt("Do you want special characters?");
+  };
+
+  if (confirmUpPassword == 'y') {
+    alphaCharacters.toUpperCase();
+  }
+
+  //Putting the choices together
+  if (confirmNumericPassword == 'y' && confirmSpecialPassword == 'y') {
+    characters = alphaCharacters.concat(numCharacters, specialCharacters);
+  } else if (confirmNumericPassword == 'y' && confirmSpecialPassword == 'n') {
+    characters = alphaCharacters.concat(numCharacters);
+  } else if (confirmNumericPassword == 'n' && confirmSpecialPassword == 'y') {
+    characters = alphaCharacters.concat(specialCharacters);
+  } else {
+    characters = alphaCharacters;
+  }
+
+  for (var i = 0; i < characters; i++){
+    
+  };
 }
 
 // Get references to the #generate element
